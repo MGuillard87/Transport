a860af79aac60d906ed687a15dafd73e7d204f40
Tsyppora <maty.la.gui@gmail.com>
2021-02-04 15:55:00 +0100

    Ajoutez des fichiers projet.

---
+++ Transport/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 |
+SecurityProtocolType.Tls;
+        }
+    }
+}
